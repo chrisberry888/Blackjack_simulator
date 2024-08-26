6665b173d7665361cca3e94198377d470f347a51
Chris Berry <chrisberry888@pop-os.mynetworksettings.com>
2024-08-25 21:35:24 -0400

    Added dicts to use instead of enums, plus added other small changes

--- src/card.py
+++ src/card.py
@@ -7,24 +7,58 @@ class Suit(Enum):
     HEARTS = 3
 
 class Value(Enum):
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9
-    TEN = 10
-    JACK = 11
-    QUEEN = 12
-    KING = 13
-    ACE = 14
-    
+    TWO = 0
+    THREE = 1
+    FOUR = 2
+    FIVE = 3
+    SIX = 4
+    SEVEN = 5
+    EIGHT = 6
+    NINE = 7
+    TEN = 8
+    JACK = 9
+    QUEEN = 10
+    KING = 11
+    ACE = 12
+
+suit_dict = {
+    0: 's', # Spades
+    1: 'd', # Diamonds
+    2: 'c', # Clubs
+    3: 'h'  # Hearts
+}
+
+value_dict = {
+    0: '2',
+    1: '3',
+    2: '4',
+    3: '5',
+    4: '6',
+    5: '7',
+    6: '8',
+    7: '9',
+    8: '10',
+    9: 'J',  # Jack
+    10: 'Q', # Queen
+    11: 'K', # King
+    12: 'A'  # Ace
+}
+
 class Card:
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
+    def __init__(self, suit_number, value_number):
+        self.suit_number = suit_number
+        self.value_number = value_number
+        self.suit = suit_dict.get(suit_number)
+        self.value = value_dict.get(value_number)
+
+    def get_card_name(self):
+        return (self.value + self.suit)
+        
 
 class Deck:
-    def __init
+    def __init__(self):
+        self.deck = []
+        for i in range(52):
+            self.deck.append(Card((i % 4), (i % 13)))
+        
+        
